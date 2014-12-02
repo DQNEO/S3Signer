bffe62bf76eb1b9c9da535cb91c93bfb97fc415c
DQNEO <dqneoo@gmail.com>
2014-12-02 14:35:23 +0900

    timestampを外から渡せるようにした。これでmockテストができる。

--- sign.php
+++ sign.php
@@ -5,7 +5,10 @@
 
 $cred = require_once __DIR__ . '/credentials.php';
 
-$expire_seconds=(60 * 5); // 5 minutes
+$now = time();
+$now = 1234567890;
+
+$expires= $now + (60 * 5); // 5 minutes later
 $endpoint='http://s3-ap-northeast-1.amazonaws.com';
 $bucket = $_GET['bucket'];
 
@@ -14,11 +17,11 @@ $objectKey=$_GET['key'];
 $mimeType=$_GET['type'];
 $acl = "public-read";
 
-echo getURL($cred['key'], $cred['secret'], $endpoint, $bucket, $objectKey, $expire_seconds, $acl, $mimeType);
+
+echo getURL($cred['key'], $cred['secret'], $endpoint, $bucket, $objectKey, $expires, $acl, $mimeType);
 
 function getURL($key, $secret, $endpoint, $bucket, $objectKey, $expires, $acl, $mimeType)
 {
-    $expires = time() + $expires;
     $amzHeaders= "x-amz-acl:" . $acl;
     $stringToSign = sprintf("PUT\n\n%s\n%s\n%s\n/%s/%s", $mimeType, $expires, $amzHeaders, $bucket, $objectKey);
 
