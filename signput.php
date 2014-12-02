af6deb1cdfdea1c7760a46192c82af4e7f815744
DQNEO <dqneoo@gmail.com>
2014-12-02 11:47:08 +0900

    extract function

--- signput.php
+++ signput.php
@@ -4,24 +4,25 @@
 //
 
 $cred = require_once __DIR__ . '/credentials.php';
-$S3_KEY=$cred['key'];
-$S3_SECRET=$cred['secret'];
 
-$S3_BUCKET='tmpdqneo';
-
-$EXPIRE_TIME=(60 * 5); // 5 minutes
-$S3_URL='http://s3-ap-northeast-1.amazonaws.com';
+$expire_seconds=(60 * 5); // 5 minutes
+$endpoint='http://s3-ap-northeast-1.amazonaws.com';
+$bucket = 'tmpdqneo';
 
 $objectName=$_GET['name'];
 
 $mimeType=$_GET['type'];
 $acl = "public-read";
 
-$expires = time() + $EXPIRE_TIME;
-$amzHeaders= "x-amz-acl:" . $acl;
-$stringToSign = sprintf("PUT\n\n%s\n%s\n%s\n/%s/%s", $mimeType, $expires, $amzHeaders, $S3_BUCKET, $objectName);
+echo getURL($cred['key'], $cred['secret'], $endpoint, $bucket, $objectName, $expire_seconds, $acl, $mimeType);
 
-$sig = urlencode(base64_encode(hash_hmac('sha1', $stringToSign, $S3_SECRET, true)));
-$url = urlencode(sprintf("%s/%s/%s?AWSAccessKeyId=%s&Expires=%s&Signature=%s", $S3_URL, $S3_BUCKET, $objectName, $S3_KEY, $expires, $sig));
+function getURL($key, $secret, $endpoint, $bucket, $objectKey, $expires, $acl, $mimeType)
+{
+    $expires = time() + $expires;
+    $amzHeaders= "x-amz-acl:" . $acl;
+    $stringToSign = sprintf("PUT\n\n%s\n%s\n%s\n/%s/%s", $mimeType, $expires, $amzHeaders, $bucket, $objectKey);
 
-echo $url;
+    $sig = urlencode(base64_encode(hash_hmac('sha1', $stringToSign, $secret, true)));
+    $url = urlencode(sprintf("%s/%s/%s?AWSAccessKeyId=%s&Expires=%s&Signature=%s", $endpoint, $bucket, $objectKey   , $key, $expires, $sig));
+    return $url;
+}
