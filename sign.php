b80ba3b75eb01720a21b2e94e2daf9ef5543da15
DQNEO <dqneoo@gmail.com>
2014-12-02 16:55:02 +0900

    refactor

--- sign.php
+++ sign.php
@@ -26,6 +26,13 @@ echo json_encode(['url' =>$url]);
 
 function getSignedURL($httpVerb, $key, $secret, $endpoint, $bucket, $objectKey, $expires, $contentType, array $amzHeaders)
 {
+    $sig = getSignature($httpVerb, $bucket, $objectKey, $amzHeaders, $contentType, $expires, $secret);
+    $url = sprintf("%s/%s/%s?AWSAccessKeyId=%s&Expires=%s&Signature=%s", $endpoint, $bucket, $objectKey   , $key, $expires, urlencode($sig));
+    return urlencode($url);
+}
+
+function getSignature($httpVerb, $bucket, $objectKey, array $amzHeaders, $contentType, $expires, $secret)
+{
     // for calculation of Signature, see
     // http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html#ConstructingTheAuthenticationHeader
     $httpVerb = "PUT";
@@ -40,7 +47,5 @@ function getSignedURL($httpVerb, $key, $secret, $endpoint, $bucket, $objectKey, 
         . $canonicalizedAmzHeaders
         . $canonicalizedResource;
 
-    $sig = base64_encode(hash_hmac('sha1', $stringToSign, $secret, true));
-    $url = sprintf("%s/%s/%s?AWSAccessKeyId=%s&Expires=%s&Signature=%s", $endpoint, $bucket, $objectKey   , $key, $expires, urlencode($sig));
-    return urlencode($url);
+    return base64_encode(hash_hmac('sha1', $stringToSign, $secret, true));
 }
